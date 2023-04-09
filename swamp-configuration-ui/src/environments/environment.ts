fa7f3f2b91faaf91004b2d25a8131e02d81aa867
Matthew Slusser <slusser12@gmail.com>
2023-04-09 10:53:36 -0400

    package-lock added

--- swamp-configuration-ui/src/environments/environment.ts
+++ swamp-configuration-ui/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  configServiceBaseUrl: "http://localhost:8080/"
+  configServiceBaseUrl: "http://localhost:8080/";
+  production: true;
 };
 
 /*
