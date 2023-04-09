21bd7c62b30557e558a24ad1d8883db1a60794e7
Matthew Slusser <slusser12@gmail.com>
2023-04-09 14:40:29 -0400

    seral input parsing and output to JSON file

--- swamp-configuration-ui/src/serial-comm.py
+++ swamp-configuration-ui/src/serial-comm.py
@@ -1,16 +1,61 @@
 #!/usr/bin/env python3
 import serial
 import time
+from typing import Optional
+import json
+
+class plantStatus(object):
+	def __init__(self, 
+	currentMoistureLevel: Optional[int] = None, 
+	currentMoisturePercent: Optional[str] = None, 
+	currentLightLevel: Optional[int] = None, 
+	lastDayWatered: Optional[int] = None, 
+	currentWaterStatus: Optional[int] = None):
+		self.currentMoisturelevel = currentMoistureLevel
+		self.currentMoisturePercent = currentMoisturePercent
+		self.currentLightLevel = currentLightLevel
+		self.lastDayWatered = lastDayWatered
+		self.currentWaterStatus = currentWaterStatus
+	
+	def parseAndPopulate(self, string):
+		parts = string.split(' = ')
+		key = parts[0]
+		value = parts[1].strip()
+		# Handle specific keys differently
+		if key == 'waterLevelStatus':
+			self.currentWaterStatus = int(value)
+		elif key == 'soilMoistureStatus':
+			self.currentMoisturelevel = int(value)
+		elif key == 'soilMoisturePercent':
+			self.currentMoisturePercent = value
+		elif key == 'Lux':
+			self.currentLightLevel = int(value)
+
+	def populated(self):
+		return self.currentMoisturelevel != None and self.currentMoisturePercent != None and self.currentLightLevel != None and self.lastDayWatered != None and self.currentWaterStatus != None
+
+	def writeJson(self):
+		with open('~/assests/data/plantStatus.json', 'w') as outfile:
+			json.dump(self.__dict__, outfile)
+
+	def __str__(self):
+		return json.dumps(self.__dict__)
 
 if __name__ == '__main__':
 	ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 	ser.reset_input_buffer()
 
 	while True:
+		plantData = plantStatus()
 		if ser.in_waiting > 0:
-			ser.write(b"Hello from Raspberry Pi! /n")
 			line = ser.readline().decode('utf-8').rstrip()
 			print(line)
 			# TODO: parse line here
+			plantData.parseAndPopulate(line)
+			if plantData.populated():
+				plantData.writeJson()
+				print(plantData)
+				ser.reset_input_buffer()
+		time.sleep(1)
 
-			time.sleep(1)
+
